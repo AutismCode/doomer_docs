45fe5a19b28390bc9081d31d81791b653e415591
AutistiCCyborg <126010501+AutistiCCyborg@users.noreply.github.com>
2023-04-12 06:06:21 +0000

    Changes to be committed:

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import MatrixBackgroundRed from './MatrixBackgroundRed';
 
 import styles from './index.module.css';
 
@@ -12,6 +13,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+      <MatrixBackgroundRed timeout={50} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
