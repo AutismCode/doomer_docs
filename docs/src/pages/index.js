508a183cfce54c9bdfcffa93abb5dd45a1b511fa
AutistiCCyborg <126010501+AutistiCCyborg@users.noreply.github.com>
2023-04-12 06:16:59 +0000

    Changes to be committed:

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -12,8 +12,8 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-                  <MatrixBackgroundRed timeout={50} />
       <div className="container">
+      <MatrixBackgroundRed timeout={50} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
