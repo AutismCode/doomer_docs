b8d37c21d4f4951a6ec17061bc37047173e6fef0
AutistiCCyborg <126010501+AutistiCCyborg@users.noreply.github.com>
2023-04-12 07:12:15 +0000

    Changes to be committed:

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -69,31 +69,7 @@ const config = {
       },
     ],
   ],
-
-  headTags: [
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: true,
-      },
-    ],
-    [
-      'link',
-      {
-        href: 'https://fonts.googleapis.com/css2?family=Chivo+Mono&display=swap',
-        rel: 'stylesheet',
-      },
-    ],
-  ],
+  
 };
 
 module.exports = config;
